3782b7da7971ad9f6dd70c84fa697543459f6035
nsider4 <115684594+nsider4@users.noreply.github.com>
2022-10-13 13:16:07 -0400

    Create Utils.java

---
+++ src/main/nsider/balancedelytras/Utils.java
@@ -0,0 +1,19 @@
+package balancedelytra;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+
+public class Utils {
+    
+    public static void sendMessage(Player p, String msg) {
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+	}
+    public static void broadcastMessage(String msg) {
+	for (Player players : Bukkit.getOnlinePlayers()) {
+            players.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+	}
+    }
+}
